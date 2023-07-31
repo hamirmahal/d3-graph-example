1e668becc59df4e709f0b392e44c1a3cfbfcd970
Hamir Mahal <hamirmahal@gmail.com>
2023-07-31 15:38:20 -0700

    chore: changes from formatting on save

--- 2ae6ac9bd33574c4@202.js
+++ 2ae6ac9bd33574c4@202.js
@@ -1,11 +1,12 @@
-function _1(md){return(
-md`# Force-directed graph
+function _1(md) {
+  return md`
+# Force-directed graph
 
-This network of character co-occurence in _Les Misérables_ is positioned by simulated forces using [d3-force](https://d3js.org/d3-force). See also a [disconnected graph](/@d3/disjoint-force-directed-graph/2), and compare to [WebCoLa](/@mbostock/hello-cola).`
-)}
+This network of character co-occurence in _Les Misérables_ is positioned by simulated forces using [d3-force](https://d3js.org/d3-force). See also a [disconnected graph](/@d3/disjoint-force-directed-graph/2), and compare to [WebCoLa](/@mbostock/hello-cola).
+  `;
+}
 
-function _chart(d3,data,invalidation)
-{
+function _chart(d3, data, invalidation) {
   // Specify the dimensions of the chart.
   const width = 928;
   const height = 600;
@@ -15,61 +16,64 @@ function _chart(d3,data,invalidation)
 
   // The force simulation mutates links and nodes, so create a copy
   // so that re-evaluating this cell produces the same result.
-  const links = data.links.map(d => ({...d}));
-  const nodes = data.nodes.map(d => ({...d}));
+  const links = data.links.map((d) => ({ ...d }));
+  const nodes = data.nodes.map((d) => ({ ...d }));
 
   // Create a simulation with several forces.
-  const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .on("tick", ticked);
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      'link',
+      d3.forceLink(links).id((d) => d.id)
+    )
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .on('tick', ticked);
 
   // Create the SVG container.
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
-      .attr("style", "max-width: 100%; height: auto;");
+  const svg = d3
+    .create('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', [0, 0, width, height])
+    .attr('style', 'max-width: 100%; height: auto;');
 
   // Add a line for each link, and a circle for each node.
-  const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
+  const link = svg
+    .append('g')
+    .attr('stroke', '#999')
+    .attr('stroke-opacity', 0.6)
     .selectAll()
     .data(links)
-    .join("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+    .join('line')
+    .attr('stroke-width', (d) => Math.sqrt(d.value));
 
-  const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
+  const node = svg
+    .append('g')
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 1.5)
     .selectAll()
     .data(nodes)
-    .join("circle")
-      .attr("r", 5)
-      .attr("fill", d => color(d.group));
+    .join('circle')
+    .attr('r', 5)
+    .attr('fill', (d) => color(d.group));
 
-  node.append("title")
-      .text(d => d.id);
+  node.append('title').text((d) => d.id);
 
   // Add a drag behavior.
-  node.call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
+  node.call(
+    d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended)
+  );
 
   // Set the position attributes of links and nodes each time the simulation ticks.
   function ticked() {
     link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+      .attr('x1', (d) => d.source.x)
+      .attr('y1', (d) => d.source.y)
+      .attr('x2', (d) => d.target.x)
+      .attr('y2', (d) => d.target.y);
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+    node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
   }
 
   // Reheat the simulation when drag starts, and fix the subject position.
@@ -101,20 +105,36 @@ function _chart(d3,data,invalidation)
   return svg.node();
 }
 
-
-function _data(FileAttachment){return(
-FileAttachment("miserables.json").json()
-)}
+function _data(FileAttachment) {
+  return FileAttachment('miserables.json').json();
+}
 
 export default function define(runtime, observer) {
   const main = runtime.module();
-  function toString() { return this.url; }
+  function toString() {
+    return this.url;
+  }
   const fileAttachments = new Map([
-    ["miserables.json", {url: new URL("./files/31d904f6e21d42d4963ece9c8cc4fbd75efcbdc404bf511bc79906f0a1be68b5a01e935f65123670ed04e35ca8cae3c2b943f82bf8db49c5a67c85cbb58db052.json", import.meta.url), mimeType: "application/json", toString}]
+    [
+      'miserables.json',
+      {
+        url: new URL(
+          './files/31d904f6e21d42d4963ece9c8cc4fbd75efcbdc404bf511bc79906f0a1be68b5a01e935f65123670ed04e35ca8cae3c2b943f82bf8db49c5a67c85cbb58db052.json',
+          import.meta.url
+        ),
+        mimeType: 'application/json',
+        toString,
+      },
+    ],
   ]);
-  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer()).define(["md"], _1);
-  main.variable(observer("chart")).define("chart", ["d3","data","invalidation"], _chart);
-  main.variable(observer("data")).define("data", ["FileAttachment"], _data);
+  main.builtin(
+    'FileAttachment',
+    runtime.fileAttachments((name) => fileAttachments.get(name))
+  );
+  main.variable(observer()).define(['md'], _1);
+  main
+    .variable(observer('chart'))
+    .define('chart', ['d3', 'data', 'invalidation'], _chart);
+  main.variable(observer('data')).define('data', ['FileAttachment'], _data);
   return main;
 }
--- README.md
+++ README.md
@@ -5,29 +5,29 @@ https://observablehq.com/@d3/force-directed-graph/2@202
 View this notebook in your browser by running a web server in this folder. For
 example:
 
-~~~sh
+```sh
 npx http-server
-~~~
+```
 
 Or, use the [Observable Runtime](https://github.com/observablehq/runtime) to
 import this module directly into your application. To npm install:
 
-~~~sh
+```sh
 npm install @observablehq/runtime@5
 npm install https://api.observablehq.com/@d3/force-directed-graph/2@202.tgz?v=3
-~~~
+```
 
 Then, import your notebook and the runtime as:
 
-~~~js
-import {Runtime, Inspector} from "@observablehq/runtime";
-import define from "@d3/force-directed-graph/2";
-~~~
+```js
+import { Runtime, Inspector } from '@observablehq/runtime';
+import define from '@d3/force-directed-graph/2';
+```
 
 To log the value of the cell named “foo”:
 
-~~~js
+```js
 const runtime = new Runtime();
 const main = runtime.module(define);
-main.value("foo").then(value => console.log(value));
-~~~
+main.value('foo').then((value) => console.log(value));
+```
